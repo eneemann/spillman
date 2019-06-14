6196d3279b0a5e339953dc0e721d6de94cdc4e25
Erik Neemann <38330621+eneemann@users.noreply.github.com>
2019-06-14 13:59:44 -0600

    Created separate ID and list bad_geom functions

--- Tools/Find_Bad_geometry.py
+++ Tools/Find_Bad_geometry.py
@@ -8,7 +8,7 @@ start_time = time.time()
 readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 print("The script start time is {}".format(readable_start))
 
-database = r"C:\E911\StGeorgeDispatch_TEST\Bad_Geometries_TEST.gdb"
+database = r"C:\E911\TOC\TOC_Staging.gdb"
 #database = r"C:\E911\StGeorgeDispatch_TEST\StGeorgeDispatch_UTM.gdb"
 env.workspace = database
 fclist = arcpy.ListFeatureClasses()
@@ -18,7 +18,22 @@ fclist = arcpy.ListFeatureClasses()
 ###############
 
 
-def find_bad_geom(fc):
+def id_bad_geom(fc):
+#    update_count = 0
+#    flist = ['SHAPE@XY', 'OBJECTID']  # for points
+    flist = ['Shape', 'OID@']  # for point, polylines, or polygons
+    bad = False
+    with arcpy.da.SearchCursor(fc, flist) as cursor:
+        print("Looping through rows in {}...".format(arcpy.Describe(fc).name))
+        for row in cursor:
+            # print row
+            if row[0][0] == None or row[0][1] == None:
+                bad = True
+                
+    if bad == True:
+        print("     {} has features with bad geometry".format(fc))            
+    
+def list_bad_geom(fc):
 #    update_count = 0
 #    flist = ['SHAPE@XY', 'OBJECTID']  # for points
     flist = ['Shape', 'OID@']  # for point, polylines, or polygons
@@ -27,14 +42,26 @@ def find_bad_geom(fc):
         for row in cursor:
             # print row
             if row[0][0] == None or row[0][1] == None:
-                print(row)
+                print(row)                
 
 ##########################
 #  Call Functions Below  #
 ##########################
 
+#for fc in fclist:
+#    desc = arcpy.Describe(fc)
+#    print("{} is of shape type: {}".format(fc, desc.shapeType))
+
 for fc in fclist:
-    find_bad_geom(fc)
+    id_bad_geom(fc)
+
+#for fc in fclist:
+#    list_bad_geom(fc)
+    
+                
+#list_bad_geom('TOC_Streets_updates_20190612')
+#list_bad_geom('zzz_Streets_10m_buffer_v2')
+#list_bad_geom('TOC_Streets_10m_buffer')
 
 print("Script shutting down ...")
 # Stop timer and print end time in UTC
