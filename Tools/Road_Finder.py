03225591914e2bf42985f49fb5ce7351f472b406
Erik Neemann <38330621+eneemann@users.noreply.github.com>
2019-07-23 13:21:07 -0600

    Edits to clean up code and comments

--- Tools/Road_Finder.py
+++ Tools/Road_Finder.py
@@ -4,13 +4,6 @@ Created on Mon Jul 22 16:10:21 2019
 @author: eneemann
 Script to detect possible new street segments by comparing new data to current data
 
-- Input Roads
-- Merge input roads into single feature
-- Buffer input roads
-- Create SGID roads layer from specific counties
-- Select SGID roads not in buffer
-- Export possible new SGID roads
-
 22 Jul 2019: Created initial version of code (EMN).
 """
 
@@ -36,22 +29,17 @@ env.overwriteOutput = True
 today = time.strftime("%Y%m%d")
 export_roads = os.path.join(staging_db, "Roads_SGID_export_" + today)
 
-where_SGID = "COUNTY_L IN ('49003')"      # Box Elder
-#arcpy.FeatureClassToFeatureClass_conversion (sgid_roads, staging_db, export_roads, where_SGID)
-
 # Create a 10m buffer around current streets data to use for selection
-#arcpy.MakeFeatureLayer_management(export_roads, "export_roads_lyr")
 roads_buff = os.path.join(staging_db, "temp_roads_buffer")
 if arcpy.Exists(roads_buff):
     arcpy.Delete_management(roads_buff)
+where_SGID = "COUNTY_L IN ('49003')"      # Box Elder
 print("Buffering {} ...".format(current_streets))
 arcpy.Buffer_analysis(current_streets, roads_buff, "10 Meters", "FULL", "ROUND", "ALL")
 
-# Need to make a layer from SGID roads feature class here
+# Select and export roads with centroids outside of the current streets buffer
 arcpy.MakeFeatureLayer_management(sgid_roads, "sgid_roads_lyr", where_SGID)
 print("SGID roads layer feature count: {}".format(arcpy.GetCount_management("sgid_roads_lyr")))
-# Select all features within 5m of current addpts FC
-fields = ['Notes']
 arcpy.SelectLayerByLocation_management("sgid_roads_lyr", "HAVE_THEIR_CENTER_IN", roads_buff,
                                                      "", "", "INVERT")
 outname = os.path.join(staging_db, "SGID_roads_to_review_" + today)
