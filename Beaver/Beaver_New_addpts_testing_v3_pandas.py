18901a91c0aa02aa74cc5f586ac46f18c0a08f15
Erik Neemann <38330621+eneemann@users.noreply.github.com>
2019-10-11 14:39:22 -0600

    major changes to sorting logic

--- Beaver/Beaver_New_addpts_testing_v3_pandas.py
+++ Beaver/Beaver_New_addpts_testing_v3_pandas.py
@@ -25,11 +25,11 @@ env.workspace = beaver_db
 env.overwriteOutput = True
 
 beaver_streets = os.path.join(beaver_db, "Streets")
-beaver_addpts = "AddressPoints_20190802"
+beaver_addpts = "AddressPoints_20191011"
 current_addpts = os.path.join(staging_db, beaver_addpts)
 
 today = time.strftime("%Y%m%d")
-new_addpts = "AddressPoints_SGID_export_20190802"
+new_addpts = "AddressPoints_SGID_export_20191011"
 possible_addpts = os.path.join(staging_db, new_addpts)
 
 # Copy current address points into a working FC
@@ -43,6 +43,49 @@ arcpy.AddField_management(working_addpts, "Street", "TEXT", "", "", 50)
 ###############
 #  Functions  #
 ###############
+
+# Calculate spelled out sufdir field to align with spillman notation
+def clean_addpts(working):
+    arcpy.management.AlterField(working, 'SuffixDir', field_length=5)
+    sufdir_count = 0
+    type_count = 0
+    # Calculate "Street" field where applicable
+#    where_clause = "STREETNAME IS NOT NULL AND STREET IS NULL"
+    #              0            1             2             3          4          5         6
+    fields = ['PrefixDir', 'StreetName', 'SuffixDir', 'StreetType', 'Street', 'AddNum', 'FullAdd']
+    with arcpy.da.UpdateCursor(working, fields) as cursor:
+        print("Looping through rows in FC ...")
+        for row in cursor:
+            if row[1] == 'MAIN' or row[1] == 'CENTER':
+                row[3] = None
+                type_count += 1
+                
+            if row[2] == 'N':
+                row[2] = 'NORTH'
+                sufdir_count += 1
+            elif row[2] == 'S':
+                row[2] = 'SOUTH'
+                sufdir_count += 1
+            elif row[2] == 'E':
+                row[2] = 'EAST'
+                sufdir_count += 1
+            elif row[2] == 'W':
+                row[2] = 'WEST'
+                sufdir_count += 1
+                
+            if row[0] is None: row[0] = ''
+            if row[2] is None: row[2] = ''
+            if row[3] is None: row[3] = ''
+            parts = [row[0], row[1], row[2], row[3]]
+            row[4] = " ".join(parts)
+            row[4] = row[4].lstrip().rstrip()
+            row[4] = row[4].replace("  ", " ").replace("  ", " ").replace("  ", " ")
+            row[6] = str(row[5]) + " " + row[4]
+            
+            cursor.updateRow(row)
+    print("Total count of updates to {0} field: {1}".format(fields[2], sufdir_count))       
+    print("Total count of updates to {0} field: {1}".format(fields[3], type_count))
+
 
 def calc_street(working):
     update_count = 0
@@ -190,21 +233,32 @@ def check_nearby_roads(working, streets, gdb):
     is_goodstreet = near_df_updated['goodstreet'] == True      # Create indexes
     # Grab rows with good streets, sort by near rank from near table, remove address point duplicates
     # This preserves the only the record with the nearest good street to the address point
-    goodstreets_df = near_df_updated[is_goodstreet].sort_values('NEAR_RANK').drop_duplicates('IN_FID')
+#    goodstreets_df = near_df_updated[is_goodstreet].sort_values('NEAR_RANK').drop_duplicates('IN_FID')
+    goodstreets_df = near_df_updated[is_goodstreet].sort_values('NEAR_RANK')
     
     # Separate rows with no good nearby street into a separate dataframe
     not_goodstreet = near_df_updated['goodstreet'] == False    # Create indexes
     # Grab rows with bad streets, sort by near rank from near table, remove address point duplicates
     # This preserves the only the record with the nearest bad street to the address point
-    badstreets_df = near_df_updated[not_goodstreet].sort_values('NEAR_RANK').drop_duplicates('IN_FID')
+#    badstreets_df = near_df_updated[not_goodstreet].sort_values('NEAR_RANK').drop_duplicates('IN_FID')
+    badstreets_df = near_df_updated[not_goodstreet].sort_values('NEAR_RANK')
     
     # Combine good and bad street dataframes, sort so good streets are at the top, then remove duplicates of address points
     # If a good streets are found, nearest one will be used; otherwise nearest bad street will be used ("near street not found")
-    filtered_df = goodstreets_df.append(badstreets_df).sort_values('goodstreet', ascending=False).drop_duplicates('IN_FID')
+#    filtered_df = goodstreets_df.append(badstreets_df).sort_values('goodstreet', ascending=False).drop_duplicates('IN_FID')
+    # Sort by multiple columns (goodstreet, then goodnum) to ensure 2nd nearest street with good num will get used
+#    filtered_df = goodstreets_df.append(badstreets_df).sort_values(['goodstreet', 'goodnum'], ascending=False).drop_duplicates('IN_FID')
+    filtered_df = goodstreets_df.append(badstreets_df).sort_values(['IN_FID','goodstreet', 'goodnum', 'edit_dist', 'NEAR_DIST'],
+                                       ascending=[True,False, False, True, True])
+    filtered_df.to_csv(r'C:\E911\Beaver Co\Addpts_working_folder\beaver_neartable_all.csv')
     # Re-sort data frame on address point ID for final data set
-    final_df = filtered_df.sort_values('IN_FID')
+    final_df = filtered_df.drop_duplicates('IN_FID')
     path = r'C:\E911\Beaver Co\Addpts_working_folder\beaver_neartable_final.csv'
     final_df.to_csv(path)
+    
+#    # Testing best method to sort data to resturn best candidate for non-matches
+#    test_df = goodstreets_df.append(badstreets_df).sort_values(['IN_FID','goodstreet', 'goodnum', 'edit_dist', 'NEAR_DIST'], ascending=[True,False, False, True, True])
+#    test_df.to_csv(r'C:\E911\Beaver Co\Addpts_working_folder\beaver_neartable_test_edit.csv')
     
     # Create new dataframe that will be used to join to address point feature class with arcpy
     join_df = final_df[['IN_FID', 'Notes', 'edit_dist']]
@@ -229,12 +283,12 @@ def check_nearby_roads(working, streets, gdb):
     # Update 'Notes' field in working address points with joined table notes
     # ArcPy makes a mess of the field names after the join, so we need to make
     # sure the proper fields are pulled and updated
-    field1 = os.path.basename(working) + "_Notes"
-    field2 = "neartable_join" + "_Notes_near"
-    fields = [field1, field2]
-    for field in fields:
-        print(field)
-#    fields = ['Notes', 'Notes_near']
+#    field1 = os.path.basename(working) + "_Notes"
+#    field2 = "neartable_join" + "_Notes_near"
+#    fields = [field1, field2]
+#    for field in fields:
+#        print(field)
+    fields = ['Notes', 'Notes_near']
     with arcpy.da.UpdateCursor(working + "_final", fields) as cursor:
         print("Looping through rows in {} to update 'Notes' field ...".format(os.path.basename(working) + "_final"))
         for row in cursor:
@@ -279,7 +333,12 @@ def logic_checks(row):
                 int(row['AddNum'].replace('-', ' ').split()[0]) >= row['R_F_ADD'] and int(row['AddNum'].replace('-', ' ').split()[0]) <= row['R_T_ADD']):
             goodnum = True
             row['Notes'] = 'no near st: likely predir or sufdir error'
-            row['goodnum'] = goodnum      
+            row['goodnum'] = goodnum
+    # Check for a good house number regardless of street name match or condition
+    if (int(row['AddNum'].replace('-', ' ').split()[0]) >= row['L_F_ADD'] and int(row['AddNum'].replace('-', ' ').split()[0]) <= row['L_T_ADD']) or (
+            int(row['AddNum'].replace('-', ' ').split()[0]) >= row['R_F_ADD'] and int(row['AddNum'].replace('-', ' ').split()[0]) <= row['R_T_ADD']):
+        goodnum = True
+        row['goodnum'] = goodnum
     return row
     
 
@@ -287,6 +346,7 @@ def logic_checks(row):
 #  Call Functions Below  #
 ##########################
 
+clean_addpts(working_addpts)
 calc_street(working_addpts)
 working_nodups = remove_duplicates(current_addpts, possible_addpts, working_addpts)
 print(arcpy.GetCount_management(working_nodups))
@@ -313,14 +373,3 @@ plt.title('Address/Street Edit Distance Histogram')
 plt.xlabel('Edit Distance')
 plt.ylabel('Count')
 plt.show()
-
-
-
-
-
-
-
-
-
-
-
