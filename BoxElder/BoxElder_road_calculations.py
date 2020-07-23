2108bdbee59edb092b0435bb1489dc0bce6b7528
Erik Neemann <eneemann@utah.gov>
2020-07-23 10:31:32 -0600

    Bug: fixed queries to catch nulls

--- BoxElder/BoxElder_road_calculations.py
+++ BoxElder/BoxElder_road_calculations.py
@@ -79,7 +79,9 @@ def calc_salias1(streets):
     update_count = 0
     # Calculate "SALIAS1" field where applicable
     # where_clause = "ALIAS1 IS NOT NULL"
-    where_clause = "ALIAS1 IS NOT NULL AND SALIAS1 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY'"
+    # where_clause = "ALIAS1 IS NOT NULL AND SALIAS1 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY'"
+    where_clause = """ALIAS1 IS NOT NULL AND SALIAS1 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY' OR
+    (ALIAS1 IS NOT NULL AND SALIAS1 IS NULL AND STREETTYPE IS NULL)"""
     fields = ['PREDIR', 'ALIAS1', 'ALIAS1TYPE', 'SALIAS1']
     with arcpy.da.UpdateCursor(streets, fields, where_clause) as cursor:
         print("Looping through rows in FC ...")
@@ -101,8 +103,9 @@ def calc_salias2(streets):
     update_count = 0
     # Calculate "SALIAS2" field where applicable
     # where_clause = "ALIAS2 IS NOT NULL"
-    where_clause = "ALIAS2 IS NOT NULL AND SALIAS2 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY'"
-
+    # where_clause = "ALIAS2 IS NOT NULL AND SALIAS2 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY'"
+    where_clause = """ALIAS2 IS NOT NULL AND SALIAS2 IS NULL AND STREETTYPE <> 'RAMP' AND STREETTYPE <> 'FWY' OR
+    (ALIAS2 IS NOT NULL AND SALIAS2 IS NULL AND STREETTYPE IS NULL)"""
     fields = ['PREDIR', 'ALIAS2', 'ALIAS2TYPE', 'SALIAS2']
     with arcpy.da.UpdateCursor(streets, fields, where_clause) as cursor:
         print("Looping through rows in FC ...")
