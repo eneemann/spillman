07c78139af353c9b87f9cbac8ec6b62da4d417cb
Erik Neemann <eneemann@utah.gov>
2020-10-02 16:18:38 -0600

    Major logic changes to handle multiple "city" names in addresses

--- San Juan/SanJuan_ALI_parse.py
+++ San Juan/SanJuan_ALI_parse.py
@@ -18,355 +18,17 @@ from matplotlib import pyplot as plt
 import re
 import timeit
 import csv
+from tqdm import tqdm
 
 # Start timer and print start time in UTC
 start_time = time.time()
 readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 print("The script start time is {}".format(readable_start))
 
-# Read in Scott's dictionary of street types
-st_types = {
-    "ALY": [
-        "ALLEE",
-        "ALLEY",
-        "ALLY",
-        "ALY"
-    ],
-    "AVE": [
-        "AV",
-        "AVE",
-        "AVEN",
-        "AVENU",
-        "AVENUE",
-        "AVN",
-        "AVNUE"
-    ],
-    "BAY": [],
-    "BLVD": [
-        "BLVD",
-        "BOUL",
-        "BOULEVARD",
-        "BOULV"
-    ],
-    "BND": [
-        "BEND",
-        "BND"
-    ],
-    "CIR": [
-        "CIR",
-        "CIRC",
-        "CIRCL",
-        "CIRCLE",
-        "CRCL",
-        "CRCLE"
-    ],
-    "COR": [
-        "COR",
-        "CORNER"
-    ],
-    "CRES": [
-        "CRECENT",
-        "CRES",
-        "CRESCENT",
-        "CRESENT",
-        "CRSCNT",
-        "CRSENT",
-        "CRSNT"
-    ],
-    "CRK": [
-        "CK",
-        "CR",
-        "CREEK",
-        "CRK"
-    ],
-    "CT": [
-        "COURT",
-        "CRT",
-        "CT"
-    ],
-    "CTR": [
-        "CEN",
-        "CENT",
-        "CENTER",
-        "CENTR",
-        "CENTRE",
-        "CNTER",
-        "CNTR",
-        "CTR"
-    ],
-    "CV": [
-        "COVE",
-        "CV"
-    ],
-    "CYN": [
-        "CANYN",
-        "CANYON",
-        "CNYN",
-        "CYN"
-    ],
-    "DR": [
-        "DRIV",
-        "DRIVE",
-        "DRV",
-        "DR"
-    ],
-    "EST": [
-        "EST",
-        "ESTATE"
-    ],
-    "ESTS": [
-        "ESTATES",
-        "ESTS"
-    ],
-    "EXPY": [
-        "EXP",
-        "EXPR",
-        "EXPRESS",
-        "EXPRESSWAY",
-        "EXPW",
-        "EXPY"
-    ],
-    "FLT": [
-        "FLAT",
-        "FLT"
-    ],
-    "FRK": [
-        "FORK",
-        "FRK"
-    ],
-    "FWY": [
-        "FREEWAY",
-        "FREEWY",
-        "FRWAY",
-        "FRWY",
-        "FWY"
-    ],
-    "GLN": [
-        "GLEN",
-        "GLN"
-    ],
-    "GRV": [
-        "GROV",
-        "GROVE",
-        "GRV"
-    ],
-    "GTWY": [
-        "GATEWAY",
-        "GATEWY",
-        "GATWAY",
-        "GTWAY",
-        "GTWY"
-    ],
-    "HL": [
-        "HILL",
-        "HL"
-    ],
-    "HOLW": [
-        "HLLW",
-        "HOLLOW",
-        "HOLLOWS",
-        "HOLW",
-        "HOLWS"
-    ],
-    "HTS": [
-        "HEIGHT",
-        "HEIGHTS",
-        "HGTS",
-        "HT",
-        "HTS"
-    ],
-    "HWY": [
-        "HIGHWAY",
-        "HIGHWY",
-        "HIWAY",
-        "HIWY",
-        "HWAY",
-        "HWY"
-    ],
-    "JCT": [
-        "JCT",
-        "JCTION",
-        "JCTN",
-        "JUNCTION",
-        "JUNCTN",
-        "JUNCTON"
-    ],
-    "LN": [
-        "LA",
-        "LANE",
-        "LANES",
-        "LN"
-    ],
-    "LNDG": [
-        "LANDING",
-        "LNDG",
-        "LNDNG"
-    ],
-    "LOOP": [
-        "LOOP",
-        "LOOPS"
-    ],
-    "MDW": [
-        "MDW",
-        "MEADOW"
-    ],
-    "MDWS": [
-        "MDWS",
-        "MEADOWS",
-        "MEDOWS"
-    ],
-    "MNR": [
-        "MANOR",
-        "MNR"
-    ],
-    "PARK": [
-        "PARK",
-        "PK",
-        "PRK",
-        "PARKS"
-    ],
-    "PASS": [
-        "PASS"
-    ],
-    "PATH": [
-        "PATH",
-        "PATHS"
-    ],
-    "PKWY": [
-        "PARKWAY",
-        "PARKWY",
-        "PKWAY",
-        "PKWY",
-        "PKY",
-        "PARKWAYS",
-        "PKWYS"
-    ],
-    "PL": [
-        "PL",
-        "PLACE"
-    ],
-    "PLZ": [
-        "PLAZA",
-        "PLZA",
-        "PLZ"
-    ],
-    "PT": [
-        "POINT",
-        "PT"
-    ],
-    "RAMP": [
-        "RAMP"
-    ],
-    "RD": [
-        "RD",
-        "ROAD"
-    ],
-    "RDG": [
-        "RDG",
-        "RDGE",
-        "RIDGE"
-    ],
-    "RNCH": [
-        "RANCH",
-        "RANCHES",
-        "RNCH",
-        "RNCHS"
-    ],
-    "ROW": [
-        "ROW"
-    ],
-    "RTE": [
-        "ROUTE"
-    ],
-    "RUN": [
-        "RUN"
-    ],
-    "SPUR": [
-        "SPUR",
-        "SPURS"
-    ],
-    "SQ": [
-        "SQ",
-        "SQR",
-        "SQRE",
-        "SQUARE",
-        "SQU"
-    ],
-    "ST": [
-        "ST",
-        "STRT",
-        "STR",
-        "STREET"
-    ],
-    "TER": [
-        "TER",
-        "TERR",
-        "TERRACE"
-    ],
-    "TRCE": [
-        "TRACE",
-        "TRACES",
-        "TRCE"
-    ],
-    "TRL": [
-        "TR",
-        "TRAILS",
-        "TRL",
-        "TRLS",
-        "TRAIL",
-        "TL"
-    ],
-    "VIS": [
-        "VIST",
-        "VISTA",
-        "VSTA",
-        "VIS",
-        "VST"
-    ],
-    "VLG": [
-        "VILL",
-        "VILLAGE",
-        "VILLG",
-        "VILLIAGE",
-        "VLG",
-        "VILLAG"
-    ],
-    "VW": [
-        "VIEW",
-        "VW"
-    ],
-    "WAY": [
-        "WAY",
-        "WY"
-    ],
-    "XING": [
-        "CROSSING",
-        "CRSSING",
-        "CRSSNG",
-        "XING"
-    ],
-    "OTHERS": [
-        "BROADWAY",
-        "MAIN",
-        "CENTER",
-        "STATE",
-        "TEMPLE",
-        "OREGON",
-        "ANGEL"
-        "NORTH",
-        "SOUTH",
-        "EAST",
-        "WEST",
-        "N",
-        "S",
-        "E",
-        "W"
-    ]
-}
 
-# Turn street types into a single list
-val = [item for item in st_types.values()]
-vals = [x for n in val for x in n]
+# # Turn street types into a single list
+# val = [item for item in st_types.values()]
+# vals = [x for n in val for x in n]
 
 today = time.strftime("%Y%m%d")
 
@@ -380,7 +42,7 @@ ali_file = r'C:\E911\1 - Utah ALI Data\archive\UT_512_edited.txt'
 ali_out = r'C:\E911\1 - Utah ALI Data\ali_out.csv'
 ali_errors = r'C:\E911\1 - Utah ALI Data\ali_errors.csv'
 
-# ali_file = r'C:\E911\1 - Utah ALI Data\sample.txt'
+# ali_file = r'C:\E911\1 - Utah ALI Data\archive\sample.txt'
 # ali_out = r'C:\E911\1 - Utah ALI Data\ali_out_test.csv'
 
 # col_names = ['FUNCTION CODE', 'TELEPHONE NUMBER', 'HOUSE #', 'PRE DIR', 'STREET NAME', 'STREET SUFFIX', 'BLANK_1',
@@ -397,6 +59,7 @@ no_city = 0
 all_good = 0
 no_ut = 0
 wireless = 0
+multi = 0
 cities = []
 with open(city_file, "r+") as file:
     city_lines = file.readlines()
@@ -414,12 +77,17 @@ with open(quads_file, "r+") as file2:
 unique_quads = set(quads)
 
 all_cities = list(set(cities + list(unique_quads)))
+all_cities = [' ' + x for x in all_cities]
 
 all_places = []
 with open(places_file, "r+") as placefile:
     allplaces = placefile.readlines()
     for item in allplaces:
         all_places.append(item.upper().strip())
+
+# Pad with leading space and remove duplicates
+all_places = list(set(all_places))
+all_places = [' ' + x for x in all_places]
 
 
 # Read raw ALI data and write out to CSV
@@ -432,10 +100,12 @@ with open(ali_file, "r+") as raw_file:
                 good.writerow(col_names)
                 bad.writerow(col_names)
                 lines = raw_file.readlines()
-                for line in lines:
+                for line in tqdm(lines):
+                    term = None
                     ut_found = False
                     city_found = False
-                    full_string = ' '.join(line.split())            
+                    full_string = ' '.join(line.split())
+
                     # print(full_string)
                     # temp2 = [part.strip() for part in full_string]
                     # print(temp2)
@@ -482,27 +152,65 @@ with open(ali_file, "r+") as raw_file:
                     possible_add = before_parts[1:]
                     possible_str = ' '.join(possible_add)
                     
+                    
+                    if ' -' in possible_str:
+                        possible_str = possible_str.split(' -', 1)[0]
+                    elif '- ' in possible_str:
+                        possible_str = possible_str.split('- ', 1)[0]
+                    elif '.' in possible_str:
+                        possible_str = possible_str.replace('.', '')
+                        
+                    # if 'HILL AFB' in possible_str:
+                    #     print(possible_str)
+                    
                     add_splitter = None
-                    for i in np.arange(len(all_cities)):          
-                        if all_cities[i] in possible_str:
-                            add_splitter = all_cities[i]
-                            addr = possible_str.rsplit(add_splitter, 1)[0]
-                            addr = addr.strip()
+                    splitters = []
+                    for i in np.arange(len(all_places)):          
+                        if all_places[i] in possible_str:
+                            splitters.append(all_places[i])
                             city_found = True
-                            break
+                            # break
                         # else:
                         #     addr = possible_str
+                    
+                    highest = 0
+                    high_index = 0
+                    if len(splitters) > 1:
+                        multi += 1
+                        # print(possible_str)
+                        # print(f'Multiple splitters found:   {splitters}')
+                        for j in np.arange(len(splitters)):
+                            term = splitters[j]
+                            result = re.search(fr"\b({term})", possible_str)
+                            beg = result.start()
+                            # print(f'Item "{splitters[j]}" starts at: {beg}')
+                            if beg > highest:
+                                highest = beg
+                                high_index = j
+                        
+                        # print(f'The highest value is:  {highest}, at splitters index {high_index}, will use "{splitters[high_index]}"')
+                        
+                    # if city_found:
+                    #     add_splitter = splitters[high_index].strip()
+                    #     addr = possible_str.rsplit(add_splitter, 1)[0]
+                    #     addr = addr.strip()
+                    # else:
+                    #     addr = possible_str
                         
                     if not city_found:
                         for i in np.arange(len(all_places)):          
                             if all_places[i] in possible_str:
-                                add_splitter = all_places[i]
+                                add_splitter = all_places[i].strip()
                                 addr = possible_str.rsplit(add_splitter, 1)[0]
                                 addr = addr.strip()
                                 city_found = True
                                 break
                             else:
-                                addr = possible_str
+                                addr = possible_str.strip()
+                    else:
+                        add_splitter = splitters[high_index].strip()
+                        addr = possible_str.rsplit(add_splitter, 1)[0]
+                        addr = addr.strip()
                     
                     if not city_found:
                         no_city += 1
@@ -559,6 +267,10 @@ with open(ali_file, "r+") as raw_file:
                     del add_splitter
                     del before_ut
                     del after_ut
+                    del splitters
+                    del highest
+                    del high_index
+                    del term
 
 
 print(f'Number of lines in ALI file:  {len(lines)}')
@@ -566,6 +278,7 @@ print(f'Number of lines with wireless call:  {wireless}')
 print(f'Number of lines where UT was not found:  {no_ut}')
 print(f'Number of lines where City was not found:  {no_city} \n')
 print(f'Number of lines where UT and City were found:  {all_good} \n')
+print(f'Number of lines with multiple splitters:  {multi} \n')
 
 # # df = pd.read_csv(ali_file, sep=' ', engine='python', header=None, names=col_names)
 # df = pd.read_csv(ali_file, sep=' ', engine='python', header=None, names=col_names, error_bad_lines=False)
@@ -581,3 +294,8 @@ print("Script shutting down ...")
 readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
 print("The script end time is {}".format(readable_end))
 print("Time elapsed: {:.2f}s".format(time.time() - start_time))
+
+
+for item in all_cities:
+    if item not in all_places:
+        print(item)
