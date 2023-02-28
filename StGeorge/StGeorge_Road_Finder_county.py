6e32b6bba6a2c7c8244b882a3d6b5f33aa690535
Erik Neemann <eneemann@utah.gov>
2023-02-28 14:38:13 -0700

    chore: update variable names

--- StGeorge/StGeorge_Road_Finder_county.py
+++ StGeorge/StGeorge_Road_Finder_county.py
@@ -21,14 +21,12 @@ print("The script start time is {}".format(readable_start))
 today = time.strftime("%Y%m%d")
 staging_db = r"C:\E911\StGeorgeDispatch\StGeorge_Staging.gdb"
 county_db = r"\\itwfpcap2\AGRC\agrc\data\county_obtained\Washington\Washington20220721.gdb"
-current_streets = os.path.join(staging_db, "StG_Streets_update_" + today)
-#current_streets = os.path.join(staging_db, "StGeorge_Dispatch_Streets")
-county_roads = os.path.join(county_db, "WashCo_RoadCenterlines")
+# current_streets = os.path.join(staging_db, "StG_Streets_update_" + today)
+current_streets = os.path.join(staging_db, "StG_Streets_update_20230214")
+# county_roads = os.path.join(county_db, "WashCo_RoadCenterlines")
+county_roads = os.path.join(staging_db, "Stg_county_roads_to_review_20230214")
 env.workspace = staging_db
 env.overwriteOutput = True
-
-# Export roads from county_db into new FC based on desired counties
-export_roads = os.path.join(staging_db, "Roads_SGID_export_" + today)
 
 # Create a 10m buffer around current streets data to use for selection
 roads_buff = os.path.join(staging_db, "temp_roads_buffer")
@@ -38,14 +36,14 @@ print("Buffering {} ...".format(current_streets))
 arcpy.Buffer_analysis(current_streets, roads_buff, "10 Meters", "FULL", "ROUND", "ALL")
 
 # Select and export roads with centroids outside of the current streets buffer
-#where_SGID = "COUNTY_L IN ('49053') OR COUNTY_R IN ('49053')"      # Washington County
-if arcpy.Exists("sgid_roads_lyr"):
-    arcpy.Delete_management("sgid_roads_lyr")
-arcpy.MakeFeatureLayer_management(county_roads, "sgid_roads_lyr")
-print("SGID roads layer feature count: {}".format(arcpy.GetCount_management("sgid_roads_lyr")))
-arcpy.SelectLayerByLocation_management("sgid_roads_lyr", "HAVE_THEIR_CENTER_IN", roads_buff, "", "", "INVERT")
-outname = os.path.join(staging_db, "SGID_roads_to_review_" + today)
-arcpy.CopyFeatures_management("sgid_roads_lyr", outname)
+#where_county = "COUNTY_L IN ('49053') OR COUNTY_R IN ('49053')"      # Washington County
+if arcpy.Exists("county_roads_lyr"):
+    arcpy.Delete_management("county_roads_lyr")
+arcpy.MakeFeatureLayer_management(county_roads, "county_roads_lyr")
+print("county roads layer feature count: {}".format(arcpy.GetCount_management("county_roads_lyr")))
+arcpy.SelectLayerByLocation_management("county_roads_lyr", "HAVE_THEIR_CENTER_IN", roads_buff, "", "", "INVERT")
+outname = os.path.join(staging_db, "county_roads_to_review_" + today)
+arcpy.CopyFeatures_management("county_roads_lyr", outname)
 
 print("Script shutting down ...")
 # Stop timer and print end time in UTC
