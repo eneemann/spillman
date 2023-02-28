8e6b92e8726997d2b1dbaba6be5ccc2585daf9e0
Erik Neemann <eneemann@utah.gov>
2023-02-28 14:32:31 -0700

    feat: update for Layton/Davis

--- Layton/Layton_jitter_CommonNames.py
+++ Layton/Layton_jitter_CommonNames.py
@@ -1,21 +1,20 @@
 # -*- coding: utf-8 -*-
 """
-Created on Fri Nov 1 07:37:17 2019
+Created on Tue Feb 7 15:37:17 2023
 
 @author: eneemann
-Script to "jitter" the coordinates of Weber Area commonplaces to ensure that
-commonplaces associated with the same address have different coordinates.
+Script to "jitter" the coordinates of Layton/Davis POIs to ensure that
+POIs associated with the same address have different coordinates.
 """
 
 import arcpy
 import os
 import time
-from datetime import datetime
 import random
 
 # Start timer and print start time in UTC
 start_time = time.time()
-readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+readable_start = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("The script start time is {}".format(readable_start))
 
 ######################
@@ -23,20 +22,20 @@ print("The script start time is {}".format(readable_start))
 ######################
 
 # Set up databases (SGID must be changed based on user's path)
-# weber_db = r"C:\E911\WeberArea\Staging103\WeberSGB.gdb"
-weber_db = r"C:\E911\WeberArea\Staging103\Weber_Staging.gdb"
+# main_db = r"C:\E911\Layton\LaytonGeoValidation.gdb"
+staging_db = r"C:\E911\Layton\Layton_staging.gdb"
 
-arcpy.env.workspace = weber_db
+arcpy.env.workspace = staging_db
 arcpy.env.overwriteOutput = True
 arcpy.env.qualifiedFieldNames = False
 
 today = time.strftime("%Y%m%d")
 
-common_names = os.path.join(weber_db, 'CommonNames_update_20201023')
-cn_update = os.path.join(weber_db, 'CommonNames_update_' + today + '_jitter')
+common_names = os.path.join(staging_db, 'PointsOfInterest_update_20201023')
+poi_update = os.path.join(staging_db, 'PointsOfInterest_update_' + today + '_jitter')
 
-print(f"Copying CommonNames to: {cn_update} ...")
-arcpy.management.Copy(common_names, cn_update)
+print(f"Copying PointsOfInterest to: {poi_update} ...")
+arcpy.management.Copy(common_names, poi_update)
 
 def jitter(val):
     new_val = val + 0.0000003*random.randint(-9,9)
@@ -44,20 +43,16 @@ def jitter(val):
 
 
 field_list = ['SHAPE@', 'SHAPE@X', 'SHAPE@Y']
-with arcpy.da.UpdateCursor(cn_update, field_list) as update_cursor:
+with arcpy.da.UpdateCursor(poi_update, field_list) as update_cursor:
     print("Looping through rows in FC ...")
     for row in update_cursor:
-#        print(f'Shape: {row[0]}')
-#        print(f'X Coord: {row[1]}')
-#        print(f'Y Coord: {row[2]}')
         row[1] = jitter(row[1])
         row[2] = jitter(row[2])
         update_cursor.updateRow(row)
-#print("Total count of blanks converted to NULLs is: {}".format(update_count))
 
 
 print("Script shutting down ...")
 # Stop timer and print end time in UTC
-readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
+readable_end = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 print("The script end time is {}".format(readable_end))
 print("Time elapsed: {:.2f}s".format(time.time() - start_time))
