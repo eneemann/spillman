ab4a7824c5b27955d1236a6f391d73505b486f8e
Erik Neemann <eneemann@utah.gov>
2023-02-28 14:31:13 -0700

    parameter changes to run code

--- UintahBasin/UintahBasin_copy_update_needs.py
+++ UintahBasin/UintahBasin_copy_update_needs.py
@@ -31,16 +31,16 @@ wgs84_db = r"C:\E911\UintahBasin\UintahBasin_WGS84.gdb"
 #            "UintahBasin_CommonPlaces": "UB_CP",
 #            "UintahBasinStreets": "UB_Streets"}
 
-FCs_dict = {"UintahBasin_CommonPlaces": "UB_CP",
-            "UintahBasinStreets": "UB_Streets"}
+# FCs_dict = {"UintahBasin_CommonPlaces": "UB_CP",
+#             "UintahBasinStreets": "UB_Streets"}
 
-#FCs_dict = {"UintahBasinCityCodes": "UB_CityCodes",
-#            "UintahBasinFire": "UB_Fire",
-#            "UintahBasinEMS": "UB_EMS",
-#            "UintahBasinLaw": "UB_Law",
-#            "UintahBasinMZ": "UB_MZ",
-#            "UintahBasin_CommonPlaces": "UB_CP",
-#            "UintahBasinStreets": "UB_Streets"}
+FCs_dict = {"UintahBasinCityCodes": "UB_CityCodes",
+            "UintahBasinFire": "UB_Fire",
+            "UintahBasinEMS": "UB_EMS",
+            "UintahBasinLaw": "UB_Law",
+            "UintahBasinMZ": "UB_MZ",
+            "UintahBasin_CommonPlaces": "UB_CP",
+            "UintahBasinStreets": "UB_Streets"}
 
 # FCs_to_project = ["address_points", "citycodes",
 #                   "common_places", "ems_zones",
