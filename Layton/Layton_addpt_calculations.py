aa513c8ffbc8affcad6a2080cdcc605e3087cffb
Erik Neemann <eneemann@utah.gov>
2023-06-01 15:34:40 -0600

    feat: cleaning, casing, calc joinid

--- Layton/Layton_addpt_calculations.py
+++ Layton/Layton_addpt_calculations.py
@@ -36,6 +36,25 @@ env.workspace = stage_db
 # total = int(result.getOutput(0))
 # print(f"addpts_lyr layer feature count: {total}")
 
+casing_replacements = {
+    'Nb ': 'NB ',
+    'Sb ': 'SB ',
+    'Eb ': 'EB ',
+    'Wb ': 'WB ',
+    'Sr ': 'SR ',
+    'Us ': 'US ',
+    'Th ': 'th ',
+    'Highway': 'Hwy'
+    }
+
+endswith_replacements = {
+    'Nb': 'NB',
+    'Sb': 'SB',
+    'Eb': 'EB',
+    'Wb': 'WB',
+    'Th': 'th',
+    }
+
 ###############
 #  Functions  #
 ###############
@@ -71,7 +90,6 @@ def blanks_to_nulls(pts):
     field_list = []
     print(f'Initial fields: {field_list}')
     for field in fields:
-        print(field.name)
         if field.name in flist:
             field_list.append(field.name)
             
@@ -107,7 +125,6 @@ def strip_fields(pts):
 
     field_list = []
     for field in fields:
-        print(field.type)
         if field.type == 'String':
             field_list.append(field.name)
             
@@ -129,12 +146,74 @@ def strip_fields(pts):
     print("Total count of stripped fields is: {}".format(update_count))
 
 
+def remove_internal_spaces(pts):
+    update_count = 0
+    # Remove extra spaces within strings
+    fields = ['Address', 'CITY', 'StreetName', 'AliasStree', 'AliasFullN', 'LocationTy', 'CommonName', 'CommonName2',
+              'CommonName3', 'CommonName4',  'CommonName5', 'LandUse', 'Orig_Addr', ]
+    with arcpy.da.UpdateCursor(pts, fields) as cursor:
+        print("Removing internal spaces on strings ...")
+        for row in cursor:
+            for i in np.arange(len(fields)):
+                if row[i] is not None:
+                    row[i] = ' '.join(row[i].split()).strip()
+                    update_count += 1
+                    cursor.updateRow(row)
+    print(f"Total count of updates: {update_count}")
+
+
+def calc_joinid(pts):
+    # Calculate "JoinID" field
+    update_count = 0
+    fields = ['JoinID', 'OID@']
+    with arcpy.da.UpdateCursor(pts, fields) as cursor:
+        print("Calculating JoinIDs ...")
+        for row in cursor:
+            row[0] = row[1]
+            update_count += 1
+            cursor.updateRow(row)
+    print(f"Total count of updates to {fields[0]} field: {update_count}")
+
+
+def apply_casing(pts):
+    # Apply title casing to street name components
+    # Exceptions: NB, SB, EB, WB
+    case_count = 0
+    
+    mixed = ['Address', 'CITY', 'PreDir', 'StreetName', 'StreetType', 'SufDir', 'AliasStree', 'AliasStr_1', 'AliasSufDi',
+              'AliasFullN', 'UnitType', 'Building', 'Unit','LocationTy', 'Notes', 'Orig_Addr']
+    upper = ['CityCode', 'CommonName', 'CommonName2', 'CommonName3', 'CommonName4',  'CommonName5', 'Exception', 'ParcelID']
+    fields = mixed + upper
+    with arcpy.da.UpdateCursor(pts, fields) as cursor:
+        print("Looping through rows to apply casing rules ...")
+        for row in cursor:
+            for i in np.arange(len(fields)):
+                if row[i] is not None and fields[i] in mixed:
+                    row[i] = row[i].title()
+                    for key in casing_replacements:
+                        if key in row[i]:
+                            row[i] = row[i].replace(key, casing_replacements[key])
+                            case_count += 1
+                    for key in endswith_replacements:
+                        if row[i].endswith(key):
+                            row[i] = row[i].replace(key, endswith_replacements[key])
+                            case_count += 1
+                elif row[i] is not None and fields[i] in upper:
+                    row[i] = row[i].upper()
+            cursor.updateRow(row)
+    print(f"Total of upper case preservations: {case_count}")
+
+
+
 ##########################
 #  Call Functions Below  #
 ##########################
 # calc_unit_from_fulladd(addpts)
 blanks_to_nulls(addpts)
 strip_fields(addpts)
+remove_internal_spaces(addpts)
+calc_joinid(addpts)
+apply_casing(addpts)
 
 # Use below to call on a selection
 # #calc_unit_from_fulladd("addpts_lyr")
